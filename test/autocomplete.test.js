cdb5a2edb81f13e42946407c81f3b83fc14e9f43
JawaraGordon <jawaragordon@icloud.com>
2023-02-04 10:42:03 -0500

    added fatchData, renderOption and fake test data to autocomplete test

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,5 +1,15 @@
 it('Shows and autocomplete', () => {
-    createAutoComplete({
-        root: document.querySelector('#target')
-    })
+  createAutoComplete({
+    root: document.querySelector('#target'),
+    fetchData() {
+      return [
+        { Title: 'Test Movie' },
+        { Title: 'Test Movie 1' },
+        { Title: 'Test Movie 2' },
+      ];
+    },
+    renderOption(movie) {
+      return movie.Title;
+    },
+  });
 });
