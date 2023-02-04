2caf56eb0430c0347e4e3daf3843ec779f29b26d
JawaraGordon <jawaragordon@icloud.com>
2023-02-04 11:51:30 -0500

    created async func to wait for debounced dropdown item to be displayed with setInterval / setTimeout

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,3 +1,21 @@
+// func to create a timer for tests
+const waitFor = (selector) => {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(() => {
+      if (document.querySelector(selector)) {
+        clearInterval(interval);
+        clearTimeout(timeout);
+        resolve();
+      }
+    }, 30);
+
+    const timeout = setTimeout(() => {
+      clearInterval(interval);
+      reject();
+    }, 2000);
+  });
+};
+
 beforeEach(() => {
   document.querySelector('#target').innerHTML = '';
   createAutoComplete({
@@ -15,10 +33,20 @@ beforeEach(() => {
   });
 });
 
-it('Shows an autocomplete', () => {
+it('Dropdown starts closed', () => {
   const dropdown = document.querySelector('.dropdown');
   //   using chai to handle assertions
   expect(dropdown.className).not.to.include('is-active');
 });
 
-it('After search, dropdown opens', () => {});
+it('After search, dropdown opens', async () => {
+  const input = document.querySelector('input');
+  input.value = 'Test Movie';
+  input.dispatchEvent(new Event('input'));
+
+  // pausing test to wait for debounce
+  await waitFor('.dropdown-item');
+
+  const dropdown = document.querySelector('.dropdown');
+  expect(dropdown.className).to.include('is-active');
+});
